a705ed5020df08d4a1926eb4d1d66de33de9ddcb
Otto Joetta <162128887+igrasilacy@users.noreply.github.com>
2024-03-04 14:16:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def get_gifs(query: str, api_key: str = giphy_api_key) -> list:
+    """
+    Get a list of URLs of GIFs based on a given query..
+    """
+    formatted_query = "+".join(query.split())
+    url = f"https://api.giphy.com/v1/gifs/search?q={formatted_query}&api_key={api_key}"
+    gifs = requests.get(url).json()["data"]
+    return [gif["url"] for gif in gifs]
+
+
+if __name__ == "__main__":
+    print("\n".join(get_gifs("space ship")))
+
+def extract_user_profile(script) -> dict:
+    """
+    May raise json.decoder.JSONDecodeError
+    """
+    data = script.contents[0]
+    info = json.loads(data[data.find('{"config"') : -1])
+    return info["entry_data"]["ProfilePage"][0]["graphql"]["user"]
